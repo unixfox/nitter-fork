c0c118c379fada2d1e499734e45633ffb21e8c3d
Emilien <4016501+unixfox@users.noreply.github.com>
2024-12-08 17:27:51 +0100

    force proxy without checking for time to load

--- public/js/proxyneededcheck.js
+++ public/js/proxyneededcheck.js
@@ -1,20 +1,17 @@
 // This check if the browser can't load pbs.twimg.com domain.
 // If it can't then we force proxy on videos and pictures.
 if (!document.cookie.includes("proxyPics") && !document.cookie.includes("proxyVideos")) {
-    var time = Date.now();
     var img = new Image();
     img.onerror = function () {
-        if ((Date.now() - time) < 100) {
-            var expires = (new Date(Date.now() + 360 * 24 * 60 * 60 * 1000)).toUTCString();
-            if (location.protocol === 'https:') {
-                document.cookie = "proxyPics=on; path=/; Secure; expires=" + expires;
-                document.cookie = "proxyVideos=on; path=/; Secure; expires=" + expires;
-            } else {
-                document.cookie = "proxyPics=on; path=/; expires=" + expires;
-                document.cookie = "proxyVideos=on; path=/; expires=" + expires;
-            }
-            location.reload();
+        var expires = (new Date(Date.now() + 360 * 24 * 60 * 60 * 1000)).toUTCString();
+        if (location.protocol === 'https:') {
+            document.cookie = "proxyPics=on; path=/; Secure; expires=" + expires;
+            document.cookie = "proxyVideos=on; path=/; Secure; expires=" + expires;
+        } else {
+            document.cookie = "proxyPics=on; path=/; expires=" + expires;
+            document.cookie = "proxyVideos=on; path=/; expires=" + expires;
         }
+        location.reload();
     };
     img.src = 'https://pbs.twimg.com/favicon.ico';
 }
